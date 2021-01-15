404b15787afac9eacf4c0906d40133c79ff722fd
justynaWoj <j.wojciechowska2011@gmail.com>
2021-01-15 19:41:34 +0100

    first commit

---
+++ src/main/java/com/kodilla/books/MainView.java
@@ -0,0 +1,27 @@
+package com.kodilla.books;
+
+import com.kodilla.books.domain.Book;
+import com.kodilla.books.service.BookService;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route
+public class MainView extends VerticalLayout {
+
+    private BookService bookService = BookService.getInstance();
+    private Grid grid = new Grid<>(Book.class);
+
+    public MainView() {
+        grid.setColumns("title", "author", "publicationYear", "type");
+        add(grid);
+        setSizeFull();
+
+        // na koncu
+        refresh();
+    }
+
+    public void refresh() {
+        grid.setItems(bookService.getBooks());
+    }
+}
