032aa6bc7a63d412f9bab92fd23b5b8bc4b6e1af
justynaWoj <j.wojciechowska2011@gmail.com>
2021-01-15 20:46:50 +0100

    Dodawanie nowych pozycji

--- src/main/java/com/kodilla/books/MainView.java
+++ src/main/java/com/kodilla/books/MainView.java
@@ -3,6 +3,7 @@ package com.kodilla.books;
 import com.kodilla.books.domain.Book;
 import com.kodilla.books.domain.BookForm;
 import com.kodilla.books.service.BookService;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -19,6 +20,8 @@ public class MainView extends VerticalLayout {
     //Filtrowanie wynikow
     private TextField filter = new TextField();
     private BookForm form = new BookForm(this);
+
+    private Button addNewBook = new Button("Add new book");
 
     public MainView() {
     /*    grid.setColumns("title", "author", "publicationYear", "type");
@@ -40,15 +43,20 @@ public class MainView extends VerticalLayout {
         filter.setValueChangeMode(ValueChangeMode.EAGER);
         filter.addValueChangeListener(e -> update());
         grid.setColumns("title", "author", "publicationYear", "type");
+
+        addNewBook.addClickListener(e -> {
+            grid.asSingleSelect().clear();
+            form.setBook(new Book());
+        });
+        HorizontalLayout toolbar = new HorizontalLayout(filter, addNewBook);
         HorizontalLayout mainContent = new HorizontalLayout(grid, form);
         mainContent.setSizeFull();
         grid.setSizeFull();
 
+        add(toolbar, mainContent);
         //ukrywa formularz
         form.setBook(null);
-        add(filter, mainContent);
         setSizeFull();
-
         refresh();
 
         grid.asSingleSelect().addValueChangeListener(event -> form.setBook((Book) grid.asSingleSelect().getValue()));
@@ -62,5 +70,4 @@ public class MainView extends VerticalLayout {
     private void update() {
         grid.setItems(bookService.findByTitle(filter.getValue()));
     }
-
 }
